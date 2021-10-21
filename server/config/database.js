a4d992c120674366c206eaa2ec5ec42fbdddee21
Ayla Ex <aylaex@protonmail.com>
2021-10-21 15:18:35 -0400

    add install script, update dependencies

--- server/config/database.js
+++ server/config/database.js
@@ -8,8 +8,6 @@ const MONGODB_URI = process.env.MONGODB_URI
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
 })
 
 // Error / success messages
